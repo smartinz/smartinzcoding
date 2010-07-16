639525f3032d2fafec5c1c15585a70bbcfd95b84
smartinz <stefano.martinz@gmail.com>
2010-07-16 10:54:20 +0200

    removed unused files

--- RhinoSecurityMgmt/Rhino.Security.Mgmt.Dtos/MappingEngineBuilder.cs
+++ RhinoSecurityMgmt/Rhino.Security.Mgmt.Dtos/MappingEngineBuilder.cs
@@ -24,24 +24,6 @@ namespace Rhino.Security.Mgmt.Dtos
 		{
 			var sl = Microsoft.Practices.ServiceLocation.ServiceLocator.Current;
 
-			Mapper.CreateMap<Rhino.Security.Model.EntitiesGroup, EntitiesGroupDto>()
-				.ForMember(d => d.StringId, o => o.MapFrom(s => sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntitiesGroup>>().ToString(s)));
-			Mapper.CreateMap<Rhino.Security.Model.EntitiesGroup, EntitiesGroupReferenceDto>()
-				.ForMember(d => d.StringId, o => o.MapFrom(s => sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntitiesGroup>>().ToString(s)))
-				.ForMember(d => d.Description, o => o.MapFrom(s => s.ToString()));
-
-			Mapper.CreateMap<Rhino.Security.Model.EntityReference, EntityReferenceDto>()
-				.ForMember(d => d.StringId, o => o.MapFrom(s => sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntityReference>>().ToString(s)));
-			Mapper.CreateMap<Rhino.Security.Model.EntityReference, EntityReferenceReferenceDto>()
-				.ForMember(d => d.StringId, o => o.MapFrom(s => sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntityReference>>().ToString(s)))
-				.ForMember(d => d.Description, o => o.MapFrom(s => s.ToString()));
-
-			Mapper.CreateMap<Rhino.Security.Model.EntityType, EntityTypeDto>()
-				.ForMember(d => d.StringId, o => o.MapFrom(s => sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntityType>>().ToString(s)));
-			Mapper.CreateMap<Rhino.Security.Model.EntityType, EntityTypeReferenceDto>()
-				.ForMember(d => d.StringId, o => o.MapFrom(s => sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntityType>>().ToString(s)))
-				.ForMember(d => d.Description, o => o.MapFrom(s => s.ToString()));
-
 			Mapper.CreateMap<Rhino.Security.Model.Operation, OperationDto>()
 				.ForMember(d => d.StringId, o => o.MapFrom(s => sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.Operation>>().ToString(s)));
 			Mapper.CreateMap<Rhino.Security.Model.Operation, OperationReferenceDto>()
@@ -78,46 +60,11 @@ namespace Rhino.Security.Mgmt.Dtos
 		{
 			var sl = Microsoft.Practices.ServiceLocation.ServiceLocator.Current;
 
-			Mapper.CreateMap<EntitiesGroupDto, Rhino.Security.Model.EntitiesGroup>()
-				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.EntitiesGroup>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntitiesGroup>>().FromString(s.StringId))
-																.ForMember(d => d.Parent, o => o.Ignore())
-
-												 .ForMember(d => d.DirectChildren, o => o.Ignore())
-
-												.ForMember(d => d.Entities, o => o.Ignore())
-
-												.ForMember(d => d.AllChildren, o => o.Ignore())
-
-												.ForMember(d => d.AllParents, o => o.Ignore())
-
-								;
-			Mapper.CreateMap<EntitiesGroupReferenceDto, Rhino.Security.Model.EntitiesGroup>()
-				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.EntitiesGroup>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntitiesGroup>>().FromString(s.StringId))
-				.ForAllMembers(o => o.Ignore());
-
-			Mapper.CreateMap<EntityReferenceDto, Rhino.Security.Model.EntityReference>()
-				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.EntityReference>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntityReference>>().FromString(s.StringId))
-																.ForMember(d => d.Type, o => o.Ignore())
-
-								;
-			Mapper.CreateMap<EntityReferenceReferenceDto, Rhino.Security.Model.EntityReference>()
-				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.EntityReference>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntityReference>>().FromString(s.StringId))
-				.ForAllMembers(o => o.Ignore());
-
-			Mapper.CreateMap<EntityTypeDto, Rhino.Security.Model.EntityType>()
-				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.EntityType>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntityType>>().FromString(s.StringId))
-												;
-			Mapper.CreateMap<EntityTypeReferenceDto, Rhino.Security.Model.EntityType>()
-				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.EntityType>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.EntityType>>().FromString(s.StringId))
-				.ForAllMembers(o => o.Ignore());
-
 			Mapper.CreateMap<OperationDto, Rhino.Security.Model.Operation>()
 				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.Operation>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.Operation>>().FromString(s.StringId))
-																				.ForMember(d => d.Parent, o => o.Ignore())
+				.ForMember(d => d.Parent, o => o.Ignore())
+				.ForMember(d => d.Children, o => o.Ignore());
 
-												.ForMember(d => d.Children, o => o.Ignore())
-
-								;
 			Mapper.CreateMap<OperationReferenceDto, Rhino.Security.Model.Operation>()
 				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.Operation>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.Operation>>().FromString(s.StringId))
 				.ForAllMembers(o => o.Ignore());
@@ -184,10 +131,10 @@ namespace Rhino.Security.Mgmt.Dtos
 						synchronizer.Associate(user, g);
 					}
 				});
+
 			Mapper.CreateMap<UserReferenceDto, Rhino.Security.Model.User>()
 				.ConstructUsing(s => string.IsNullOrEmpty(s.StringId) ? sl.GetInstance<Nexida.Infrastructure.IFactory<Rhino.Security.Model.User>>().Create() : sl.GetInstance<Nexida.Infrastructure.IStringConverter<Rhino.Security.Model.User>>().FromString(s.StringId))
 				.ForAllMembers(o => o.Ignore());
-
 		}
 	}
 }
--- RhinoSecurityMgmt/Rhino.Security.Mgmt/Views/Home/Index.aspx
+++ RhinoSecurityMgmt/Rhino.Security.Mgmt/Views/Home/Index.aspx
@@ -11,66 +11,36 @@
 	<script type="text/javascript" src="ext/ext-all-debug-w-comments.js"></script>
 	<script type="text/javascript" src="ext/TabCloseMenu.js"></script>
 	<script type="text/javascript" src="js/Rpc.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupGridPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceGridPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeGridPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationGridPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupGridPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserGridPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupJsonReader.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceJsonReader.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeJsonReader.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationJsonReader.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupJsonReader.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserJsonReader.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupSearchPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceSearchPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeSearchPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationSearchPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupSearchPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserSearchPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupSearchFormPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceSearchFormPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeSearchFormPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationSearchFormPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupSearchFormPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserSearchFormPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupField.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceField.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeField.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationField.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupField.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserField.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserListField.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupFormPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceFormPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeFormPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationFormPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupFormPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserFormPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.MainViewPort.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupColumn.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceColumn.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeColumn.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationColumn.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupColumn.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserColumn.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroup.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReference.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityType.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.Operation.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.Permission.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroup.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.User.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupEditWindow.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceEditWindow.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeEditWindow.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationEditWindow.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupEditWindow.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserEditWindow.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntitiesGroupEditPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityReferenceEditPanel.js"></script>
-	<script type="text/javascript" src="js/Rhino.Security.EntityTypeEditPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.OperationEditPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UsersGroupEditPanel.js"></script>
 	<script type="text/javascript" src="js/Rhino.Security.UserEditPanel.js"></script>
@@ -88,6 +58,7 @@
 
 		Ext.onReady(function () {
 			Ext.QuickTips.init();
+			Rpc.init();
 			var mainViewport = new Rhino.Security.MainViewport();
 		});
 		</script>
